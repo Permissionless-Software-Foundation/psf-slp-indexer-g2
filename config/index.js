bc40a373b13a8b0f65dcc9cb322b086a3b6dcb7a
Chris Troutner <chris.troutner@gmail.com>
2025-10-24 07:12:41 -0700

    fix(tribeOS): Addin TribeOS token to black list

--- config/index.js
+++ config/index.js
@@ -18,7 +18,9 @@ export default {
     ? []
     : [
         // FlexUSD
-        'dd21be4532d93661e8ffe16db6535af0fb8ee1344d1fef81a193e2b4cfa9fbc9'
+        'dd21be4532d93661e8ffe16db6535af0fb8ee1344d1fef81a193e2b4cfa9fbc9',
+        // TribeOS
+        '0df768b5485c72645de069b68f66d02205c26f827c608ef5ffa976266d753d50'
       ]
 
 }
